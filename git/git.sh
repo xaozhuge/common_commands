9b734a55d41a0e3d867f2efd74032f1e992f6258
zzz <xaozhuge@163.com>
2024-02-29 11:16:39 +0800

    git.sh

---
+++ git/git.sh
@@ -0,0 +1,14 @@
+一、查看用户、邮箱
+1. 查看当前用户 
+git config user.name
+2. 查看当前邮箱 
+git config user.email
+
+
+二、修改用户、邮箱
+1. 修改当前用户 
+git config --global user.name "用户名"
+git config --local user.name "用户名"
+2. 修改当前邮箱 
+git config --global user.email "邮箱" 
+git config --local user.email "邮箱"
