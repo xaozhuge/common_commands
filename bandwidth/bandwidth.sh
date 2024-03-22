b4b4c072fa42c02394ec647b622c199f8d80a6ec
zzz <xaozhuge@163.com>
2024-03-22 23:22:09 +0800

    bandwidth.sh

---
+++ bandwidth/bandwidth.sh
@@ -0,0 +1,15 @@
+#1. 安装iftop
+yum -y install iftop
+
+#2. 查看服务器网卡
+ifconfig
+
+#3. 查看指定网卡的服务，例如华为云使用 eth0
+iftop -i eth0
+
+#4. 查看带宽
+/usr/sbin/iftop -i eth0 
+
+#5. 新增带宽统计脚本
+touch checkbandwidth.sh
+chmod +x checkbandwidth.sh
