fd83620a54b90a899a530783534df5c60af7f93e
zzz <xaozhuge@163.com>
2024-03-29 23:41:05 +0800

    nginx.sh

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,5 +1,8 @@
-1. 查询 nginx 日志的时间和接口
+# 1. 查询 nginx 日志的时间和接口
 cat 2024-02-29.log|awk '{print $2, $8}'|awk -F '##' '{print $2, $3}'
 
-2. 查询 nginx 日志的时间大于3秒
+# 2. 查询 nginx 日志的时间大于3秒
 cat 2024-02-29.log|awk '{print $2, $8}'|awk -F '##' '$2 >= 3'
+
+# 3. nginx重启
+sudo nginx -s reload
