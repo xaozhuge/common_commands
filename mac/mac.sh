c76906cbffd0b69f307eae3be445980c976084df
zzz <xaozhuge@163.com>
2024-04-02 18:40:57 +0800

    mac.sh

--- mac/mac.sh
+++ mac/mac.sh
@@ -2,3 +2,5 @@
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.php@5.6.plist
 # 2. 关闭自动重启
 launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.php@5.6.plist
+# 3. mac的环境变量加载顺序
+/etc/profile /etc/paths ~/.bash_profile
