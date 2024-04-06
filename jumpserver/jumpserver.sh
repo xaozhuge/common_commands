bab9ba2293e2c02024994a1cee4b63a8f820c684
zzz <xaozhuge@163.com>
2024-04-06 23:38:22 +0800

    jumpserver.sh

---
+++ jumpserver/jumpserver.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "请先配置 server_ip、add_date"
+echo "默认 server_ip=127.0.0.1"
+echo "默认 add_date=20240406"
+server_ip=127.0.0.1
+add_date=20240406
+
+echo "一、添加没有sudo权限的用户"
+echo "例如 sudo useradd nosudo"$server_ip
+echo "例如 sudo passwd nosudo"$server_ip
+echo "例如 密码参考 md5(nosudo#"$add_date"?四位随机字母)"
+
+
+
+
+
