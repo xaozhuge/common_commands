180566fbb950764593236d1aad8dbbaeafc80e6c
zzz <xaozhuge@163.com>
2024-04-07 16:50:22 +0800

    disk_mount.sh

---
+++ disk/disk_mount.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "当前系统的磁盘有: fdisk -l|grep 'Disk /'"
+fdisk -l|grep 'Disk /'
+
+echo "已经分区的磁盘有: fdisk -l|grep 'Linux'"
+fdisk -l|grep 'Linux'
+
+echo "1. 使用 fdisk 对数据盘进行分区，将数据盘分为一个主分区"
+echo "例如 华为云: fdisk /dev/vdb"
+echo "例如 华为云: Command (m for help): n"
+echo "例如 华为云: Select (default p): "
+echo "例如 华为云: Partition number (1-4, default 1): "
+echo "例如 华为云: First sector (2048-419430399, default 2048):"
+echo "例如 华为云: Last sector, +sectors or +size{K,M,G} (2048-419430399, default 419430399): "
+echo "例如 华为云: Command (m for help): w"
+
+echo "2. 格式化分区"
+echo "例如 华为云: mkfs -t ext4 /dev/vdb1"
+
+
+
