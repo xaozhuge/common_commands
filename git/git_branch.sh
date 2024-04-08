bb491cdd12e025ae7c17c167eefadc31553bcca5
zzz <xaozhuge@163.com>
2024-04-08 11:10:36 +0800

    git_branch.sh

--- git/git_branch.sh
+++ git/git_branch.sh
@@ -2,6 +2,8 @@
 git branch -r|awk -F '/' '{print $2"/"$3}'|grep keywords
 git branch |awk -F '/' '{print $1"/"$2}'|grep keywords
 
+# 2. 批量删除含有关键字的远程/本地分支
+git branch -r|awk -F '/' '{print $2"/"$3}'|grep keywords|head -n 80|xargs git push origin --delete
 
 
 
