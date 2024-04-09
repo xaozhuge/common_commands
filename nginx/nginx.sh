fa1c39807ace2d87c484f034b3ab122a7082cd4b
zzz <xaozhuge@163.com>
2024-04-09 10:55:48 +0800

    nginx.sh

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,5 +1,6 @@
 # 1. 查询 nginx 日志的时间和接口
-cat 2024-02-29.log|awk '{print $2, $8}'|awk -F '##' '{print $2, $3}'
+now=`date "+%Y-%m-%d"`
+cat $now.log|awk '{print $2, $8}'|awk -F '##' '{print $2, $3}'
 
 # 2. 查询 nginx 日志的时间大于3秒
 cat 2024-02-29.log|awk '{print $2, $8}'|awk -F '##' '$2 >= 3'
