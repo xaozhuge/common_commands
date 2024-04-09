434426dea6eb4dc960a481b2cc75867cb1e13136
zzz <xaozhuge@163.com>
2024-04-09 10:56:23 +0800

    nginx.sh

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -3,7 +3,7 @@ now=`date "+%Y-%m-%d"`
 cat $now.log|awk '{print $2, $8}'|awk -F '##' '{print $2, $3}'
 
 # 2. 查询 nginx 日志的时间大于3秒
-cat 2024-02-29.log|awk '{print $2, $8}'|awk -F '##' '$2 >= 3'
+cat $now.log|awk '{print $2, $8}'|awk -F '##' '$2 >= 3'
 
 # 3. nginx重启
 sudo nginx -s reload
