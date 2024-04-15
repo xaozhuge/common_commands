fc95d145a68b269748187222b87153063b146ba2
zzz <xaozhuge@163.com>
2024-04-15 13:53:34 +0800

    kafka.sh

--- kafka/kafka.sh
+++ kafka/kafka.sh
@@ -6,6 +6,8 @@
 ./kafka-console-consumer.sh --bootstrap-server 173.16.0.62:9092 --topic example
 # 4. 从开始位置消费，并写入文件
 ./kafka-console-consumer.sh --bootstrap-server 173.16.0.62:9092 --from-beginning --topic example > temp.log
+# 5. 查看所有消费组详情
+./kafka-consumer-groups.sh --bootstrap-server 173.16.0.62:9092 --describe --all-groups
 
 
 
