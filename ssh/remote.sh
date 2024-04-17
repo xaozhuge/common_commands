a4bd248a85b4041855beed5430341161b233444a
zzz <xaozhuge@163.com>
2024-04-17 15:28:51 +0800

    remote.sh

--- ssh/remote.sh
+++ ssh/remote.sh
@@ -12,7 +12,15 @@ for info in $content; do
     echo "${array[0]}:${array[4]}(${array[2]}|${array[3]})"
     # (1):测试服务器(39.1.1.1|10.1.1.1)
 done
+echo
+echo
+echo
 
-echo
-echo
+read -p "where code you will go?": NUM
+
+read user ip_public <<< `cat $server_list |grep "(${NUM})|"|awk -F '|' '{print $2,$3}'`
+
+ssh $user@$ip_public
+# ssh zzz@39.1.1.1
+
 
