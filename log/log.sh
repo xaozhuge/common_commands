406020003c7962c6ba52b9ac4ad9fe7d45f8355b
zzz <xaozhuge@163.com>
2024-04-21 20:21:43 +0800

    log.sh

--- log/log.sh
+++ log/log.sh
@@ -1,7 +1,8 @@
-一、
-1、> /dev/null 实际是  1>/dev/null 的缩写，表示标准输出重定向到空设备文件
-2、1表示stdout 标准输出，系统默认值是1
-3、2表示stderr 标准错误
+# 1. > /dev/null 说明
+> /dev/null 实际是  1>/dev/null 的缩写，表示标准输出重定向到空设备文件
+1表示stdout 标准输出，系统默认值是1
+2表示stderr 标准错误
+
 4、2>&1 表示 2的输出重定向等同于1
 
 # 二、清理日志，并保证继续产生日志
@@ -9,3 +10,5 @@ sudo sh -c 'echo "" > /var/log/cron'
 
 # 三、判断上条执行的命令
 [ $? -eq 0 ] && echo "success" || echo "fail"
+
+
