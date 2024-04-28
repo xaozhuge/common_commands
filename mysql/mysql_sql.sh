27ebcee357c4008b49673ca8d710e5f8e0796270
zzz <xaozhuge@163.com>
2024-04-28 15:14:42 +0800

    查询表的所有字段

--- mysql/mysql_sql.sh
+++ mysql/mysql_sql.sh
@@ -2,7 +2,8 @@
 SELECT * FROM information_schema.processlist order by TIME desc
 kill id
 
-
+# 2. 查询表的所有字段
+SELECT GROUP_CONCAT(column_name) FROM information_schema. COLUMNS where table_name = ''
 
 
 
