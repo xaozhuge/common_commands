a3c534cfa9cd80c81baee072af89c6a00340a91e
zzz <xaozhuge@163.com>
2024-05-09 13:06:37 +0800

    alias_bak.sh

--- alias/alias_bak.sh
+++ alias/alias_bak.sh
@@ -6,6 +6,9 @@ echo "服务器
 1:重启 nginx/1.22.1
 2:重启 php-fpm/5.5.7
 3:前往nginx配置文件
+4:查看开机启动项
+5:前往php日志、慢查询目录
+6:前往nginx日志目录
 "
 read -p "where code you will go?": NUM
 
@@ -18,6 +21,17 @@ case $NUM in
     ;;
     3)
     cd /alidata/server/nginx/conf/vhosts;
+    pwd;
+    ;;
+    4)
+    cat /etc/rc.local
+    ;;
+    5)
+    cd /alidata/log/php;
+    pwd;
+    ;;
+    6)
+    cd /alidata/log/nginx/access;
     pwd;
     ;;
 esac
