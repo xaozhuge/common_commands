14195034f560cdea2f091736b98973c915b41948
zzz <xaozhuge@163.com>
2024-05-11 15:22:40 +0800

    linux_startup.sh

--- linux/linux_startup.sh
+++ linux/linux_startup.sh
@@ -11,3 +11,15 @@
 /etc/init.d/php-fpm73 start
 /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
 /usr/local/bin/redis-server /alidata/server/redis/conf/redis.conf &
+
+# 3. 说明 该命令用于启动 vsftpd（Very Secure FTP Daemon），
+# 一个用于 Linux 系统的轻量级、高性能的 FTP 服务器软件
+/usr/sbin/vsftpd
+
+# 二、开机没自启原因
+# 1. 服务器没成功是因为都没有加执行权限, 需要给 /etc/rc.d/rc.local 加执行权限
+chmod +x /etc/rc.d/rc.local
+
+# 2. 重启验证是否可以正常开启 
+reboot
+
