8a96e4e3369ac1d6021602d417c888014ad6a0d1
zzz <xaozhuge@163.com>
2024-05-12 22:10:23 +0800

    alias_make.sh

--- alias/alias_make.sh
+++ alias/alias_make.sh
@@ -1,3 +1,8 @@
 # 一、Linux全局配置别名
 # 1. sudo -i 切换到root账号
 # 2. 查看 /etc/bashrc 是否已有别名
+# 3. 执行如下命令
+# demo1
+echo "alias test='cd /alidata/www/test.com/demo1'" >> /etc/bashrc
+# demo2
+echo "alias bak='source /alidata/www/script.com/bak.sh'" >> /etc/bashrc
