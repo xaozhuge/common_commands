cd084741fd0b86b1797d9445bdb7ba2cfc04437b
zzz <xaozhuge@163.com>
2024-05-14 11:37:13 +0800

    sed.sh

--- sed/sed.sh
+++ sed/sed.sh
@@ -3,3 +3,9 @@ sed -i '/aaa/d' 1.php
 
 # 2. 删除从第一行到10000行
 sed -i '1,10000d' filename
+
+# 3. sed 替换 
+# 3.1 Mac        
+sed -i '' 's/before/after/g' 文件名
+# 3.2 Linux
+sed -i 's/before/after/g' 文件名
