57e6347aac1dc9a4d268d86e76929a881ad1bf75
zzz <xaozhuge@163.com>
2024-05-20 17:54:29 +0800

    docker_build.sh

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -3,4 +3,7 @@
 1.2 -f Dockerfile 
 	-f 是选项标志，后面跟着文件名 Dockerfile，使用名为 
 	Dockerfile 的文件作为构建镜像的配置文件
-
+1.3 -t test:test
+	-t 是另一个选项标志，后面跟着镜像的名称和标签。
+	test:test 表示将要构建的镜像名称为 test，标签也为 test。
+	标签可以帮助您区分不同的镜像版本。
