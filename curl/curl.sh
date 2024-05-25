9e45e82054b588fa3db1e7e5c414873bae5a8cb0
zzz <xaozhuge@163.com>
2024-05-25 23:38:27 +0800

    curl.sh

--- curl/curl.sh
+++ curl/curl.sh
@@ -1,6 +1,9 @@
 # 1. 指定 请求方法为 POST
 curl -X POST
 
+# 2. 设置 Content-Type 为 application/json 或者 application/x-www-form-urlencoded
+curl -H "Content-Type: application/json"
+curl -H "Content-Type: application/x-www-form-urlencoded"
 
 
 
