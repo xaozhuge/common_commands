d3870c25f1b38ec863f4960a7e88f683925a1525
zzz <xaozhuge@163.com>
2024-05-28 17:24:56 +0800

    curl.sh

--- curl/curl.sh
+++ curl/curl.sh
@@ -8,3 +8,8 @@ curl -H "Content-Type: application/x-www-form-urlencoded"
 # 3. 使用 -d 选项来指定请求的表单数据 或者 指定 JSON 数据
 curl -d "key1=value1&key2=value2"
 curl -d '{"key1":"value1", "key2":"value2"}'
+
+# 4. 使用 --data-urlencode 选项自动对传递的数据进行 URL 编码，包括转义特殊字符，例如 /
+curl --data-urlencode "path=controller/method"
+
+
