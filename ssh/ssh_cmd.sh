5dec6c53b78bb91388e57415d45f2c3d3634c65f
zzz <xaozhuge@163.com>
2024-05-31 09:27:32 +0800

    ssh_cmd.sh

--- ssh/ssh_cmd.sh
+++ ssh/ssh_cmd.sh
@@ -12,3 +12,9 @@ EOF
 
 # 2. ssh 远程执行命令
 ssh root@192.168.1.1 "cd /alidata/www;ls"
+
+# 3. ssh远程执行命令无法使用 awk （无法使用 $）
+# 只要保证 ssh使用的是 ""，并且awk中的 $加上转义即可
+ssh root@192.168.1.1 "cat 1.php|awk '{print \$1}'"
+
+
