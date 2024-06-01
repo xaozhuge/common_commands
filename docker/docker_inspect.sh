551d9118b1fbdde4420700705b3857a591c6e87a
zzz <xaozhuge@163.com>
2024-06-01 11:37:19 +0800

    docker_inspect.sh

--- docker/docker_inspect.sh
+++ docker/docker_inspect.sh
@@ -8,3 +8,8 @@ docker inspect <volume_id_or_name>
 # 2. --format 选项用于提取特定的字段, 而不是返回完整的JSON输出.
 # --format 使用Go模板语法
 docker inspect --format='{{.LogPath}}' php56_c
+
+# 3. 获取docker容器的日志路径
+docker inspect php56_c|grep -i log
+
+
