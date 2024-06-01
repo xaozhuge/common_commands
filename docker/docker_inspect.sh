db847ca781c7d0fe9cc53a1fb6737aa8f58230d1
zzz <xaozhuge@163.com>
2024-06-01 11:36:58 +0800

    docker_inspect.sh

--- docker/docker_inspect.sh
+++ docker/docker_inspect.sh
@@ -4,3 +4,7 @@ docker inspect <container_id_or_name>
 docker inspect <image_id_or_name>
 docker inspect <network_id_or_name>
 docker inspect <volume_id_or_name>
+
+# 2. --format 选项用于提取特定的字段, 而不是返回完整的JSON输出.
+# --format 使用Go模板语法
+docker inspect --format='{{.LogPath}}' php56_c
