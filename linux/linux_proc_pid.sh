ff78778f721b5778d63887daad48d1240f0bd8ea
zzz <xaozhuge@163.com>
2024-06-02 16:32:36 +0800

    linux_proc_pid.sh

--- linux/linux_proc_pid.sh
+++ linux/linux_proc_pid.sh
@@ -5,3 +5,7 @@
 # 3. /proc/[pid]/exe 符号链接, 指向进程的可执行文件路径
 ls -al /proc/31500/exe
 readlink /proc/31500/exe
+
+# 4. /proc/[pid]/cmdline 包含进程启动时的命令行参数, 以NUL字符分隔
+cat /proc/31500/cmdline
+cat /proc/31500/cmdline | tr '\0' ' '
