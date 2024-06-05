289b8e7e4d6f2d970acefc48440b345937e485d4
zzz <xaozhuge@163.com>
2024-06-05 09:57:49 +0800

    checkcanal.sh

--- canal/checkcanal.sh
+++ canal/checkcanal.sh
@@ -11,3 +11,14 @@ isnotice=/alidata/canal/script/isnotice
 
 # 定义关键字数组
 KEYWORDS=(" ERROR ")
+
+# 函数：检查行是否包含关键字
+contains_keyword() {
+    local line="$1"
+    for keyword in "${KEYWORDS[@]}"; do
+        if [[ "$line" == *"$keyword"* ]]; then
+            return 0  # 找到关键字，返回 true
+        fi
+    done
+    return 1  # 没有找到关键字，返回 false
+}
