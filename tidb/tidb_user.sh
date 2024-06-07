667c9567247f429cc27aa6708daf16b238616885
zzz <xaozhuge@163.com>
2024-06-07 10:13:46 +0800

    tidb_user.sh

--- tidb/tidb_user.sh
+++ tidb/tidb_user.sh
@@ -3,6 +3,8 @@ CREATE USER '用户名'@'localhost' IDENTIFIED BY '密码';
 
 # 2. 新增用户权限
 GRANT ALL PRIVILEGES ON 数据库.数据库表 TO '用户名'@'localhost';
-
+# 2.1 用户名 可读权限:readdb 可写权限:writedb 测试权限:testdb
+# 2.2 注意:部分user的名称限制16个字符
+GRANT SELECT ON 数据库.* TO '用户名+服务器IP'@'服务器IP';
 
 
