8802a0b83343669be2f33a854bd2b99efc01f3bc
zzz <xaozhuge@163.com>
2024-06-08 15:54:54 +0800

    docker_image_operate.sh

--- docker/docker_image_operate.sh
+++ docker/docker_image_operate.sh
@@ -5,3 +5,7 @@ docker save -o nginx_221102 nginx:1.22
 # 2. 导入镜像 
 # docker load < localname
 docker load < nginx_221102
+
+# 3. 为Docker镜像添加一个新的标签
+docker tag source_image[:tag] target_image[:tag]
+
