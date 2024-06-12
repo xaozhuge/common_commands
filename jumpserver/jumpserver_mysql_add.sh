2d1d746f8917b4a3c3586ebd42fa35e7bd3a9c36
zzz <xaozhuge@163.com>
2024-06-12 09:44:10 +0800

    jumpserver_mysql_add.sh

--- jumpserver/jumpserver_mysql_add.sh
+++ jumpserver/jumpserver_mysql_add.sh
@@ -4,3 +4,8 @@ server_ip=server_ip
 dbname=dbname
 passwd=passwd
 proxyip=proxyip
+
+echo "1、添加线上只读权限的用户-未存在账号"
+echo "grant SELECT on "$dbname".* to 'readdb"$server_ip"'@'"$proxyip"' identified by '"$passwd"';"
+echo "2、添加线上只读权限的用户-已存在账号"
+echo "grant SELECT on "$dbname".* to 'readdb"$server_ip"'@'"$proxyip"';"
