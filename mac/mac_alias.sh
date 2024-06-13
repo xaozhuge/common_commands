c6394c8d1a73f8f6a1047177625fbf1545569129
zzz <xaozhuge@163.com>
2024-06-13 09:25:22 +0800

    mac_alias.sh

--- mac/mac_alias.sh
+++ mac/mac_alias.sh
@@ -2,3 +2,8 @@
 /etc/profile
 ~/.bash_profile
 
+
+# 2. 编辑写入和echo写入
+alias 'du1=du -sh * 2>/dev/null | sort -h'
+echo "alias du1=du -sh * 2>/dev/null | sort -h" >> ~/.bash_profile
+echo "alias du1=du -sh * 2>/dev/null | sort -h" >> /etc/profile
