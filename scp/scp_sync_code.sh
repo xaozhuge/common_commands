3cd9a467f0afcd82c7a486f40be48eed172cca35
zzz <xaozhuge@163.com>
2024-06-14 11:19:01 +0800

    scp_sync_code.sh

--- scp/scp_sync_code.sh
+++ scp/scp_sync_code.sh
@@ -9,3 +9,16 @@ LOCAL_DIR="本地项目目录"
 # 获取变更的文件列表
 changed_files=$(git status --porcelain | awk '{print $2}')
 
+# 同步变更的文件
+for file in $changed_files; do  
+  # 使用 scp 同步文件
+  scp $LOCAL_DIR/$file $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR/$file
+  
+  # 检查 scp 命令是否成功执行
+  if [ $? -ne 0 ]; then
+    echo "Error : $file"
+    exit 1
+  fi
+done
+
+echo "All successfully"
