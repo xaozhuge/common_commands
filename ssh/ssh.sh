6e6cbf8f317448bbf36a103d73e15cd6f2eb3c0c
zzz <xaozhuge@163.com>
2024-06-17 15:37:23 +0800

    ssh.sh

--- ssh/ssh.sh
+++ ssh/ssh.sh
@@ -4,12 +4,7 @@ ssh-copy-id -i ~/.ssh/id_rsa.pub 用户名@IP
 # 2. ssh -p 指定端口（默认端口为22 可以忽略）
 ssh -p 端口 root@192.168.1.1
 
-# 3. ssh 远程执行命令
-ssh root@192.168.1.1 "cd /alidata/www;ls"
-
-# 4. ssh远程执行命令无法使用 awk （无法使用 $）
-# 只要保证 ssh使用的是 ""，并且awk中的 $加上转义即可
-ssh root@192.168.1.1 "cat 1.php|awk '{print \$1}'"
-
-
-
+# 3. ssh 和 ssh -t 和 ssh -tt
+# 3.1 ssh 用于基本的 SSH 连接和远程命令执行。
+ssh user@hostname
+ssh user@hostname "ls -l"
