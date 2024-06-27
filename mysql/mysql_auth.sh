814ce0d4c9c37a170ca4271e8dc09859fb7b10ed
zzz <xaozhuge@163.com>
2024-06-27 09:27:06 +0800

    mysql_auth.sh

--- mysql/mysql_auth.sh
+++ mysql/mysql_auth.sh
@@ -1,7 +1,9 @@
 # 1. 添加账号
 grant all privileges on *.* to 'username'@'%' identified by 'password';
+
 # 2. 添加只读账号
-grant SELECT on DB.* to 'read_only_DB'@'IP' identified by 'password';
+GRANT SELECT ON 数据库名称.* TO 'readdb+数据库IP'@'堡垒机IP' identified by 'password';
+GRANT SELECT ON 数据库名称.* TO 'readdb+数据库IP'@'堡垒机IP';
+
 # 3. 用于让 用户的修改和权限的更改 立即生效
 flush privileges;
-
