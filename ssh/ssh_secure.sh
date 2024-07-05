95553a81f73495cf66b524511a41730dece26ee3
zzz <xaozhuge@163.com>
2024-07-05 10:01:49 +0800

    ssh_secure.sh

--- ssh/ssh_secure.sh
+++ ssh/ssh_secure.sh
@@ -2,6 +2,7 @@
 cat /var/log/secure* |grep "Accepted password"
 cat /var/log/secure* |grep "Accepted publickey"
 cat /var/log/secure* |grep "Failed password"
+cat /var/log/secure* |grep "Failed password"|wc -l
 
 # 2. 记录并移除 "用户 from IP", 验证登录服务器的用户和IP
 cat /var/log/secure* |grep "Accepted password"|grep -v "用户 from IP"
