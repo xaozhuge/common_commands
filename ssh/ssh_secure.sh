e65f0f403cb499eeb80b8791ca45d58068e77d6c
zzz <xaozhuge@163.com>
2024-07-05 10:01:26 +0800

    ssh_secure.sh

--- ssh/ssh_secure.sh
+++ ssh/ssh_secure.sh
@@ -1,6 +1,7 @@
 # 1. 查看安全登录日志(密码登录和密钥登录)
 cat /var/log/secure* |grep "Accepted password"
 cat /var/log/secure* |grep "Accepted publickey"
+cat /var/log/secure* |grep "Failed password"
 
 # 2. 记录并移除 "用户 from IP", 验证登录服务器的用户和IP
 cat /var/log/secure* |grep "Accepted password"|grep -v "用户 from IP"
