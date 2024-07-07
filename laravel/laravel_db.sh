3c385fa4295cb6ac72413f83e184fe7a4ade9192
zzz <xaozhuge@163.com>
2024-07-07 23:19:21 +0800

    laravel_db.sh

--- laravel/laravel_db.sh
+++ laravel/laravel_db.sh
@@ -1,5 +1,6 @@
 # 1. where语句设置
 ->where('id', 1)
+->where('id',[1, 2])
 ->where('name', '!=', 'test')
 
 # 2. first() 方法返回的是一个对象(Eloquent 模型或 stdClass 对象)
@@ -9,4 +10,5 @@
 ->toArray()
 
 # 4. pluck 方法将获取匹配条件的所有记录中id字段的值, 并以数组的形式返回
-->pluck('id')
+->pluck('id')->toArray()
+
