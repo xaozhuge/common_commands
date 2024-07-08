e7f37d15e93472bf818c1cdf4b273513d4dc8bfe
zzz <xaozhuge@163.com>
2024-07-08 09:17:39 +0800

    mac_remote.sh

---
+++ mac/mac_remote.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# 表注释
+# CREATE TABLE `server` (
+#   `id` int(11) NOT NULL AUTO_INCREMENT,
+#   `user` varchar(255) DEFAULT '' COMMENT '用户名',
+#   `ip_public` varchar(50) DEFAULT '' COMMENT '公网IP',
+#   `ip_private` varchar(50) DEFAULT '' COMMENT '内网IP',
+#   `remark` varchar(255) DEFAULT '' COMMENT '服务器备注',
+#   `display_flag` tinyint(1) DEFAULT '1' COMMENT '是否显示 1是0否',
+#   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+#   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+#   PRIMARY KEY (`id`)
+# ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='服务器管理';
