360d306ea326ac7c61be56dbc31275d78b497362
zzz <xaozhuge@163.com>
2024-07-10 09:30:08 +0800

    mysql_insert.sh

--- mysql/mysql_insert.sh
+++ mysql/mysql_insert.sh
@@ -2,3 +2,7 @@
 insert into to_table_name
 (column1, column2, column3)
 select columna, columnb, columnc from database_name.from_table_name
+
+# 2. 快速查询一个表有哪些字段, 并且是逗号分隔
+SELECT GROUP_CONCAT(column_name) FROM information_schema.COLUMNS where table_name = ''
+SELECT GROUP_CONCAT(column_name) FROM information_schema.COLUMNS where table_name = '' and table_schema = ''
