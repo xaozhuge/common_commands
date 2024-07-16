e8c5935fac91464fc16b471cea8bfd0c2380cbee
zzz <xaozhuge@163.com>
2024-07-16 09:21:06 +0800

    crontab.sh

--- crontab/crontab.sh
+++ crontab/crontab.sh
@@ -2,4 +2,6 @@
 32 16 02 01 * /alidata/source.sh >> /alidata/tmp.log 2>&1
 # 2. 以某个用户的身份执行脚本
 sudo -u www /alidata/server/php73/bin/php
+# 3. root部署(方便管理), 以www运行(避免权限问题)
+* * * * * sudo -u www /alidata/server/php73/bin/php
 
