d3a63c9c228d64807e09550764aee8894a56658a
zzz <xaozhuge@163.com>
2024-07-19 09:23:39 +0800

    laravel.sh

--- laravel/laravel.sh
+++ laravel/laravel.sh
@@ -1,9 +1,9 @@
-一、Please provide a valid cache path.
-1. 确保storage目录下有如app，framework，views三个目录。
+# 一、Please provide a valid cache path.
+# 1. 确保storage目录下有如app, framework, views三个目录
 sudo mkdir -p storage/app
 sudo mkdir -p storage/views
 sudo mkdir -p storage/framework/cache
 sudo mkdir -p storage/framework/sessions
 sudo mkdir -p storage/framework/views
+sudo chmod -R  777 storage
 
-二、
