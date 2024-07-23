32a09a674811360ece5f53e0d1bfe0c5a721c58d
zzz <xaozhuge@163.com>
2024-07-23 09:26:20 +0800

    linux_var.sh

--- linux/linux_var.sh
+++ linux/linux_var.sh
@@ -6,7 +6,7 @@ var=`pwd`
 
 # 3. eval 是一个内建的 shell 命令, 用于将传递给它的字符串作为 shell 命令来执行
 
-# 4. 数组,数组下的内容
+# 4. 数组,数组下的内容, 数组赋值,使用小括号(...)创建一个数组
 array=($(ls))
 # 4.1 输出数组的第一个元素
 echo ${array[0]}
@@ -14,3 +14,6 @@ echo ${array[0]}
 echo ${array[*]}
 echo ${array[@]}
 
+# 5. 参数替换
+# 5.1 表示将变量 var 中所有匹配 pattern 的部分替换为 replacement
+${var//pattern/replacement}
