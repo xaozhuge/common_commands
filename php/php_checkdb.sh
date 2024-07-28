93c460307db2cb05d0b7371c1c65b6ae8a1b3cc6
zzz <xaozhuge@163.com>
2024-07-28 21:38:22 +0800

    php_checkdb.sh

--- php/php_checkdb.sh
+++ php/php_checkdb.sh
@@ -3,3 +3,8 @@
 $db = array(
     ["数据库IP","数据库用户名称","数据库用户密码","数据库端口","数据库名称","SQL语句","数据库说明[数据库IP]"],
 );
+
+foreach ($db as $v) {
+    list($servername, $username, $password, $port, $dbname, $sql, $content) = $v;
+    checkdb($servername, $username, $password, $port, $dbname, $sql, $content);
+}
