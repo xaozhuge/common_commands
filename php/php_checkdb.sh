150d3b6bcfc22b763d671eee8b77dd3adf51c504
zzz <xaozhuge@163.com>
2024-07-28 21:40:07 +0800

    php_checkdb.sh

--- php/php_checkdb.sh
+++ php/php_checkdb.sh
@@ -8,3 +8,29 @@ foreach ($db as $v) {
     list($servername, $username, $password, $port, $dbname, $sql, $content) = $v;
     checkdb($servername, $username, $password, $port, $dbname, $sql, $content);
 }
+
+# 校验测试数据库
+echo "所有检查结束". PHP_EOL;
+
+function checkdb($servername, $username, $password, $port, $dbname, $sql, $content){
+    echo $content.PHP_EOL;
+    try {
+        $dsn = "mysql:host=$servername;port=$port;dbname=$dbname";
+        $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+
+        // 创建 PDO 实例
+        $conn = new PDO($dsn, $username, $password, $options);
+        echo "1. 数据库连接成功".PHP_EOL;
+
+        
+    } catch (PDOException $e) {
+        echo "1. 数据库连接失败: " . $e->getMessage();
+    }
+    echo PHP_EOL;
+    echo PHP_EOL;
+    // 关闭连接
+    $conn = null;
+}
+
