35a646d467ae4e4352ecb4c5fd4643fa501f3eb1
zzz <xaozhuge@163.com>
2024-07-31 09:22:30 +0800

    laravel.sh

--- laravel/laravel.sh
+++ laravel/laravel.sh
@@ -10,3 +10,5 @@ sudo chmod -R  777 storage
 # 2. 定时任务
 * * * * * sudo -u www /alidata/server/php73/bin/php /alidata/www/script.com/artisan user:update
 
+# 3. 找出时间异常的SQL
+tail -f laravel.log | awk -F 'Time: ' '{split($2, a, ","); if (a[1] > 1000) print $0}'
