e48c2ec3828c790e84ddf3b7a07f7f0f9b8a229e
zzz <xaozhuge@163.com>
2024-07-31 09:23:58 +0800

    jenkins_checkdb.sh

---
+++ jenkins/jenkins_checkdb.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo ""
+echo '服务器说明[公网IP/内网IP]' 
+ssh -tt 服务器账号@服务器IP 'sudo /alidata/server/php/bin/php /root/script/checkdb.php;\
+echo -n "php-fpm:";ps aux|grep php-fpm|wc -l;\
+echo -n "负载:";w|grep load;\
+echo -n "内存:";free|grep Mem|awk "{print \$3/\$2 * 100}";\
+echo -n "CPU:";top -bn1| grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk "{print 100 - \$1}"; \
+echo -n "带宽:"; sudo iftop -i eth1 -t -s 2 -nN  2>&1|grep "send rate";\
+echo -n "带宽:"; sudo iftop  -t -s 2 -nN  2>&1 | egrep "=>|<="|xargs -l2 |awk "{print \$8,\$4,\$5,\$6}"|grep -v "192.168.0";\
+echo;'
+echo ''
