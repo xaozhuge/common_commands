4d5f3d6a4071ae93f64a301366c31aac55e77422
zzz <xaozhuge@163.com>
2024-08-02 09:28:05 +0800

    python_download.sh

--- python/python_download.sh
+++ python/python_download.sh
@@ -1,3 +1,9 @@
 # 1. 查看 python 版本
 python --version
 
+# 2. 根据python版本, 启动临时的http服务器
+# 2.1 python3 8000端口可以修改
+python -m http.server 8000
+# 2.2 python2 8000端口可以修改
+python -m SimpleHTTPServer 8000
+
