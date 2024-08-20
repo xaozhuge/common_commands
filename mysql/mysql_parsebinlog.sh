9f391e4538a3079aa57c0c3f6650e52134baae84
zzz <xaozhuge@163.com>
2024-08-20 09:20:44 +0800

    mysql_parsebinlog.sh

--- mysql/mysql_parsebinlog.sh
+++ mysql/mysql_parsebinlog.sh
@@ -7,3 +7,8 @@ filename=`ls -lt /alidata/server/mysql/data/ | grep 'mysql-bin.0' |head -n 1|awk
 tmp_file=/tmp/tmp.sql
 runtime_log=/alidata/www/script.com/runtime_log
 
+#生成分钟前的日志
+/alidata/server/mysql-5.6.21/bin/mysqlbinlog --no-defaults --base64-output=decode-rows -v -v --start-datetime="$start_time" --stop-datetime="$end_time" $binlog_dir$filename > $tmp_file
+#分析分钟前的日志
+echo $title >> $runtime_log
+echo "`cat $tmp_file|grep '###'|grep 'DELETE\|UPDATE\|INSERT' | awk -F '.' '{print $2}'|sort -r | uniq -c | sort -n -r| head -n 10`" >> $runtime_log
