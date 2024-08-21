9ea497ba580c5efc7894f5ed298580ea2e208103
zzz <xaozhuge@163.com>
2024-08-21 09:22:28 +0800

    mysql_mysqldump.sh

---
+++ mysql/mysql_mysqldump.sh
@@ -0,0 +1,4 @@
+# 1. --databases 指定要导出的一个或多个数据库. 使用该选项, mysqldump会在生成的SQL文件中包含创建数据库(CREATE DATABASE)的语句, 并且会在每个数据库的前面使用(USE database_name)语句
+# [database_name1] [database_name2] 列出要备份的数据库名称. 可以指定多个数据库名称, 每个数据库名称之间用空格分开
+mysqldump --databases [database_name1] [database_name2]
+
