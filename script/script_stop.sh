07653d202e6b26fdcac24cbbecf1477c5c92d1cd
zzz <xaozhuge@163.com>
2024-09-01 23:08:40 +0800

    script_stop.sh

--- script/script_stop.sh
+++ script/script_stop.sh
@@ -6,3 +6,8 @@ nowtime=`date "+%Y-%m-%d %H:%M:%S"`
 log=runtimelog
 pid=$(ps aux|grep "python3 main.py"|grep -v 'grep'|awk '{print $2}')
 
+if [[ -z "$pid" ]]; then
+    echo ${nowtime}" 未发现 '$pid' 存在, 程序未运行" >> $log
+    echo "" >> $log
+    exit
+fi
