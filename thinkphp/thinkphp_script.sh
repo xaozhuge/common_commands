1144bf85d96e6f7357a16915115b4c7c5dc5bdd0
zzz <xaozhuge@163.com>
2024-09-02 11:55:41 +0800

    thinkphp_script.sh

--- thinkphp/thinkphp_script.sh
+++ thinkphp/thinkphp_script.sh
@@ -9,3 +9,17 @@ if [[ $count -gt 100 ]]; then
 	echo "脚本超载运行" >> ${logpath}${action}-${date}.log
 	exit;
 fi
+
+if [[ ! -d $logpath ]]; then
+	mkdir -p $logpath
+fi
+
+if [[ "$time" > "02:00:00" ]] && [[ "$time" < "04:00:00" ]]; then
+	echo '当前时间段数据库正在备份不提供脚本服务'  >> ${logpath}${action}-${date}.log 2>&1
+fi
+
+/alidata/server/php/bin/php /alidata/www/script/api_script/index.php ${control}/${action} >> ${logpath}${action}-${date}.log 2>&1
+
+# 定时任务案例
+# 1 * * * * /alidata/www/script/thinkphp_script.sh testcon testfunc
+
