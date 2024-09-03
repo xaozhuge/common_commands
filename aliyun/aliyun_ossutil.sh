acd65fba21a643a8c2c56c7fde8dd6eb683976f2
zzz <xaozhuge@163.com>
2024-09-03 09:44:38 +0800

    aliyun_ossutil.sh

--- aliyun/aliyun_ossutil.sh
+++ aliyun/aliyun_ossutil.sh
@@ -3,3 +3,6 @@ wget http://gosspublic.alicdn.com/ossutil/1.7.14/ossutil64
 chmod +x ossutil64
 sudo mv ossutil64 /usr/local/bin/ossutil
 ossutil --version
+
+# 2. 使用 ossutil 配置阿里云OSS访问密钥和其他信息
+ossutil config
