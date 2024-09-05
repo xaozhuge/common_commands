ca55f11287624b04d965168a14fdce061e8f45bf
zzz <xaozhuge@163.com>
2024-09-05 09:25:29 +0800

    nginx_install.sh

--- nginx/nginx_install.sh
+++ nginx/nginx_install.sh
@@ -4,3 +4,8 @@ cd /hwdata/server/
 wget http://nginx.org/download/nginx-1.22.1.tar.gz
 tar -zxvf nginx-1.22.1.tar.gz
 
+# 2. 安装 nginx,
+cd /hwdata/server/nginx-1.22.1/
+./configure --prefix=/hwdata/server/nginx-122/  --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-http_gunzip_module --with-http_gzip_static_module
+make && make install
+
