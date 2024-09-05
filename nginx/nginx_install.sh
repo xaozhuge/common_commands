fa7fa320af0d859a284abcc232e46f568c3f601c
zzz <xaozhuge@163.com>
2024-09-05 09:25:54 +0800

    nginx_install.sh

--- nginx/nginx_install.sh
+++ nginx/nginx_install.sh
@@ -9,3 +9,6 @@ cd /hwdata/server/nginx-1.22.1/
 ./configure --prefix=/hwdata/server/nginx-122/  --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-http_gunzip_module --with-http_gzip_static_module
 make && make install
 
+# 3. 出现问题: ./configure: error: the HTTP rewrite module requires the PCRE library.
+sudo yum install pcre-devel
+
