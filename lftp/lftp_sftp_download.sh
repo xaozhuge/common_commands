4ec0fdfb33d561af81a5d7df555b06c3f3e3dfbb
zzz <xaozhuge@163.com>
2024-09-09 09:20:43 +0800

    lftp_sftp_download.sh

--- lftp/lftp_sftp_download.sh
+++ lftp/lftp_sftp_download.sh
@@ -5,3 +5,12 @@ IP=
 PORT=
 nowdate=`date +%Y%m01`
 desdirfirst=/upload/$(date -d "$nowdate last day" +%Y-%m-%d)
+
+srcdir=/alidata/www/upload/csvfile/$(date +%Y-%m)'-01'
+if [ ! -d $srcdir ];then
+	mkdir $srcdir
+fi
+
+base_info=${desdirfirst}/base_info.csv
+new_media=${desdirfirst}/new_media.csv
+
