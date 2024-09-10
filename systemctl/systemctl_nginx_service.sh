aeaf527a842cd6e116f724255773db254256de3a
zzz <xaozhuge@163.com>
2024-09-10 09:20:50 +0800

    systemctl_nginx_service.sh

--- systemctl/systemctl_nginx_service.sh
+++ systemctl/systemctl_nginx_service.sh
@@ -1,3 +1,13 @@
 [Unit]
 Description=A high performance web server and a reverse proxy server
 After=network.target
+
+[Service]
+Type=forking
+PIDFile=/hwdata/server/nginx-122/logs/nginx.pid
+ExecStartPre=/hwdata/server/nginx-122/sbin/nginx -t -c /hwdata/server/nginx-122/conf/nginx.conf
+ExecStart=/hwdata/server/nginx-122/sbin/nginx -c /hwdata/server/nginx-122/conf/nginx.conf
+ExecReload=/hwdata/server/nginx-122/sbin/nginx -s reload
+ExecStop=/hwdata/server/nginx-122/sbin/nginx -s stop
+PrivateTmp=true
+
