afc4cd2a8bbb9ed25e47d2fab5ffc45653764fae
zzz <xaozhuge@163.com>
2024-09-16 23:17:04 +0800

    docker_install.sh

--- docker/docker_install.sh
+++ docker/docker_install.sh
@@ -3,3 +3,7 @@ yum update
 yum install -y yum-utils device-mapper-persistent-data lvm2
 
 
+# 2. 向 YUM 的配置文件中添加新的软件源(仓库)
+yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+ls /etc/yum.repos.d/
+
