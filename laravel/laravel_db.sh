3ef359cdb2e3c8ef03275ee4639a269514972f3b
zzz <xaozhuge@163.com>
2024-09-26 09:10:49 +0800

    laravel_db.sh

--- laravel/laravel_db.sh
+++ laravel/laravel_db.sh
@@ -1,7 +1,7 @@
 # 1. where语句设置
 ->where('id', 1)
-->where('id',[1, 2])
 ->where('name', '!=', 'test')
+->whereIn('id',[1, 2])
 
 # 2. first() 方法返回的是一个对象(Eloquent 模型或 stdClass 对象)
 ->first()
@@ -24,4 +24,9 @@ DB::table('表名')->where()
 
 # 8. 更新数据
 ->update($update_data)
+
+# 9. query() 是用于手动获取查询构造器实例
+# 调用 where() 时, Laravel 会自动创建一个查询构造器实例, 因此你不需要显式地调用 query() 方法
+Model::query()->where('id', $id)->value('name');
+Model::where('id', $id)->value('name');
 
