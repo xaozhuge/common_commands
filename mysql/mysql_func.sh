8af3210d2f9f9f97a68974dde6551d289eb4ecc6
zzz <xaozhuge@163.com>
2024-09-26 09:10:08 +0800

    mysql_func.sh

--- mysql/mysql_func.sh
+++ mysql/mysql_func.sh
@@ -4,3 +4,10 @@ SUBSTRING_INDEX(str, delim, count)
 # 1.2 如果count是负数, 函数返回从右到左的所有部分, 直到分隔符出现count次为止
 host 的值为 127.0.0.1:3306
 SELECT SUBSTRING_INDEX(host, ':', 1) AS ip
+
+# 2. LAST_DAY() 是MySQL的一个日期函数, 用于返回给定日期所在月份的最后一天
+# 获取当前月份的最后一天
+SELECT LAST_DAY(CURDATE());
+# 获取特定日期的最后一天
+SELECT LAST_DAY('2024-02-15');
+
