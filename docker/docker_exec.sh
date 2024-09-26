c1694aa6abd58df295a64d2228816c3007782518
zzz <xaozhuge@163.com>
2024-09-26 09:09:39 +0800

    docker_exec.sh

--- docker/docker_exec.sh
+++ docker/docker_exec.sh
@@ -3,3 +3,7 @@ docker exec 容器名/容器ID Linux命令
 docker exec kafka_c sh -c "cd /data/kafka/bin; ./kafka-topics.sh --list --bootstrap-server 127.0.0.1:9092"
 docker exec nginx122_c nginx -s reload
 
+# 2. 使用 docker exec 的 --user 选项指定运行命令的用户 ID(UID)和组 ID(GID)
+docker exec php73_c whoami
+docker exec --user 1000:1000 php73_c whoami
+
