8aca5635b64dd0275845c87c1e4089dca7389bad
zzz <xaozhuge@163.com>
2024-09-30 21:04:12 +0800

    mysql_slave_check.sh

--- mysql/mysql_slave_check.sh
+++ mysql/mysql_slave_check.sh
@@ -11,3 +11,7 @@ port=
 #慢查询结果
 array=($(mysql -h$host -u$user -P$port -p$password -e "show slave status\G"|grep "Slave_IO_Running:\|Slave_SQL_Running:\|Seconds_Behind_Master:"))
 
+echo Slave_IO_Running ':' ${array[1]}
+echo Slave_SQL_Running ':' ${array[3]}
+echo Seconds_Behind_Master ':' ${array[5]}
+
