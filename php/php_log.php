10621f496c3263af12ba5672fefd3977f698fd75
zzz <xaozhuge@163.com>
2024-10-09 09:29:43 +0800

    php_log.php

--- php/php_log.php
+++ php/php_log.php
@@ -16,3 +16,30 @@ $username="";
 $password="";
 $sql="";
 
+
+try {
+    $dsn = "mysql:host=$servername;port=$port;dbname=$dbname";
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+
+    // 创建 PDO 实例
+    $conn = new PDO($dsn, $username, $password, $options);
+
+    // 执行 SQL 查询
+    $stmt = $conn->query($sql);
+
+    // 输出数据
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $source = $row['source'];
+        $method = $row['method'];
+        $post = json_decode($row['post'], true);
+        $url = $map[$source]. $method;
+        $param_str=http_build_query($post);
+        break;
+    }
+
+} catch (PDOException $e) {
+    echo "1. 数据库连接失败: " . $e->getMessage();
+}
+
