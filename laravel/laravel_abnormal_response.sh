84ef9ee6f1c7a08391e5e262bdc1f755ed4a9f68
zzz <xaozhuge@163.com>
2024-10-13 10:26:33 +0800

    laravel_abnormal_response.sh

--- laravel/laravel_abnormal_response.sh
+++ laravel/laravel_abnormal_response.sh
@@ -1,3 +1,12 @@
 # 1. 异常返回值记录log
 app/Exceptions/Error.php
 
+# 2. 根据状态码为非0
+if($this->code != 0){
+    $request = app('request');
+    $log_data['ip'] = getip();
+    $log_data['post'] = $request->all();
+    $log_data['method'] = $request->route()->uri();
+    loga(returnJson([$result, $log_data]), "message");
+}
+
