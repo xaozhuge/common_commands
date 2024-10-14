8043a029e5229ea18ea95d72b4451ba5b6dbaa70
zzz <xaozhuge@163.com>
2024-10-14 14:09:41 +0800

    php_time.sh

--- php/php_time.sh
+++ php/php_time.sh
@@ -1,3 +1,7 @@
 # 1. 返回东八区的时间, 使用 gmdate 而不是 date
 $time = gmdate('Y-m-d H:i:s', time() + 8*3600);
 
+# 2. 返回东八区的时间戳
+$time = gmdate('Y-m-d H:i:s', time() + 8*3600);
+$timestamp = strtotime($time);
+
