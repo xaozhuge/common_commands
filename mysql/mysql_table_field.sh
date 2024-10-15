2e8b27c93fc5ebec1672dc814a2191dc7ed077eb
zzz <xaozhuge@163.com>
2024-10-15 09:34:57 +0800

    mysql_table_field.sh

--- mysql/mysql_table_field.sh
+++ mysql/mysql_table_field.sh
@@ -1,7 +1,10 @@
 # 1. 添加字段
-ALTER TABLE 表名 ADD 字段名 字段类型 NOT NULL DEFAULT '默认值' comment '字段说明' after 字段名;
-ALTER TABLE 表名 ADD 字段名 varchar(255) NOT NULL DEFAULT '' comment '字段说明';
+ALTER TABLE 表名 ADD 字段名 字段类型 NOT NULL DEFAULT '默认值' COMMENT '字段说明' after 字段名;
+ALTER TABLE 表名 ADD 字段名 varchar(255) NOT NULL DEFAULT '' COMMENT '字段说明';
 
 # 2. 删除字段
-ALTER TABLE 表名 drop column 字段名;
+ALTER TABLE 表名 DROP COLUMN 字段名;
+
+# 3. 修改字段
+ALTER TABLE 表名 MODIFY 字段名 字段类型 DEFAULT '默认值' COMMENT '字段说明';
 
