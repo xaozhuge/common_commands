0fe089488a6f4b440295e761a58d01833380e7f5
zzz <xaozhuge@163.com>
2024-10-17 09:35:09 +0800

    php-fpm_conf.sh

--- php-fpm/php-fpm_conf.sh
+++ php-fpm/php-fpm_conf.sh
@@ -11,3 +11,8 @@ access.log = /alidata/log/php/php-fpm7.access.log
 # 4. 配置 php-fpm 的访问日志格式
 access.format = "%{REMOTE_ADDR}e %d %t %s %m %{SERVER_NAME}e %{REQUEST_URI}e %C%% %{HTTP_ORIGIN}e"
 
+# 5. 定义 PHP-FPM 可以同时运行的最大子进程数（即处理请求的 PHP 进程）
+# 限制了同时处理的请求数量. 如果请求数超过此值, 额外的请求将被排队, 直到有可用的子进程
+# 有助于防止服务器资源耗尽, 确保在高负载情况下的稳定性
+pm.max_children = 100
+
