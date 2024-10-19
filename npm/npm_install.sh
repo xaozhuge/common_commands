ba23f781a01551c8a8f33598e84bd0d9d50ecbaa
zzz <xaozhuge@163.com>
2024-10-19 15:57:52 +0800

    npm_install.sh

--- npm/npm_install.sh
+++ npm/npm_install.sh
@@ -1,3 +1,7 @@
 # 1. -g 表示 全局安装, 意味着安装的工具将在系统范围内可用, 而不仅限于当前项目
 npm install -g n --registry=https://registry.npmmirror.com
 
+# 2. n 表示 安装的包的名称
+# n 是一个 Node.js 版本管理器, 可以安装和切换不同版本的 Node.js
+npm install -g n --registry=https://registry.npmmirror.com
+
