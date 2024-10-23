2f6afe16c25887f851f1ac78886976f1b46bbdb4
zzz <xaozhuge@163.com>
2024-10-23 09:12:35 +0800

    mysql_sql.sh

--- mysql/mysql_sql.sh
+++ mysql/mysql_sql.sh
@@ -1,6 +1,7 @@
 # 1. 查看mysql 进程 并 杀掉进程
 SELECT * FROM information_schema.processlist order by TIME desc
 kill id
+SELECT * FROM information_schema.processlist where info <> ''  order by TIME desc
 
 # 2. 查询表的所有字段
 SELECT GROUP_CONCAT(column_name) FROM information_schema. COLUMNS where table_name = ''
