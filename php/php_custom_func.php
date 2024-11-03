21ac540b822697554eea1d15d50c4732bf7ccedc
zzz <xaozhuge@163.com>
2024-11-03 22:40:31 +0800

    php_custom_func.php

--- php/php_custom_func.php
+++ php/php_custom_func.php
@@ -9,3 +9,13 @@ function returnJson($list){
     return json_encode($list, JSON_UNESCAPED_UNICODE);
 }
 
+# 3. thinkphp打印日志
+function pr($data, $save_name = ''){
+    $root      = dirname($_SERVER['SCRIPT_FILENAME']);
+    $path      = $_SERVER['PATH_INFO'];
+    $save_name = dv($save_name, getpath());
+    $file_name = $root. '/Application/Runtime/Logs/Home/'. $save_name;
+    $content   = print_r($data, true);
+    file_put_contents($file_name,date('Y-m-d H:i:s'). "\n". $content. "\n",FILE_APPEND);
+}
+
