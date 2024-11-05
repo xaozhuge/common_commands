5efe2608eacf4ba0812aa8d6c09c128faf431c8f
zzz <xaozhuge@163.com>
2024-11-05 10:01:23 +0800

    mysql_insert.sh

--- mysql/mysql_insert.sh
+++ mysql/mysql_insert.sh
@@ -6,3 +6,8 @@ select columna, columnb, columnc from database_name.from_table_name
 # 2. 快速查询一个表有哪些字段, 并且是逗号分隔
 SELECT GROUP_CONCAT(column_name) FROM information_schema.COLUMNS where table_name = ''
 SELECT GROUP_CONCAT(column_name) FROM information_schema.COLUMNS where table_name = '' and table_schema = ''
+
+# 3. 插入数据
+# 3.1 写入完整字段-完整而且有顺序的列
+insert into table_name values (字段值);
+
