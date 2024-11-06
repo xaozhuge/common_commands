a903b2b47e94a63b9757f687f54a98fc90bd167f
zzz <xaozhuge@163.com>
2024-11-06 09:15:29 +0800

    mysql_insert.sh

--- mysql/mysql_insert.sh
+++ mysql/mysql_insert.sh
@@ -10,4 +10,6 @@ SELECT GROUP_CONCAT(column_name) FROM information_schema.COLUMNS where table_nam
 # 3. 插入数据
 # 3.1 写入完整字段-完整而且有顺序的列
 insert into table_name values (字段值);
+# 3.2 写入完整字段-列名完整可以无序-对应的列值
+insert into table_name (字段名) values (字段值);
 
