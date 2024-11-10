3a28b97bbcc5341455e69aab810da67ab8a695f7
zzz <xaozhuge@163.com>
2024-11-10 22:48:13 +0800

    php-fpm_conf.sh

--- php-fpm/php-fpm_conf.sh
+++ php-fpm/php-fpm_conf.sh
@@ -7,6 +7,7 @@ error_log = /alidata/log/php/php-fpm7.log
 
 # 3. 配置 php-fpm 的访问日志路径
 access.log = /alidata/log/php/php-fpm7.access.log
+access.log = /alidata/log/php/php-fpm5.access.log
 
 # 4. 配置 php-fpm 的访问日志格式
 access.format = "%{REMOTE_ADDR}e %d %t %s %m %{SERVER_NAME}e %{REQUEST_URI}e %C%% %{HTTP_ORIGIN}e"
