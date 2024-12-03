0678b11842b8e9ceb930bcd12e2a72fdc3fff807
zzz <xaozhuge@163.com>
2024-12-03 09:27:23 +0800

    disk_fdisk.sh

--- disk/disk_fdisk.sh
+++ disk/disk_fdisk.sh
@@ -1,3 +1,6 @@
 # 1. Disk /dev/vdd 表示 云盘的设备名称
 Disk /dev/vdd: 1181.1 GB, 1181116006400 bytes, 2306867200 sectors
 
+# 2. 2306867200 sectors 表示 云盘容量= 2306867200 * 512 bytes = 1100GB
+Disk /dev/vdd: 1181.1 GB, 1181116006400 bytes, 2306867200 sectors
+
