3d8662f6ceabe137b707ae044c9bc3e454864e6b
zzz <xaozhuge@163.com>
2024-12-04 16:24:31 +0800

    disk_fdisk.sh

--- disk/disk_fdisk.sh
+++ disk/disk_fdisk.sh
@@ -4,3 +4,6 @@ Disk /dev/vdd: 1181.1 GB, 1181116006400 bytes, 2306867200 sectors
 # 2. 2306867200 sectors 表示 云盘容量= 2306867200 * 512 bytes = 1100GB
 Disk /dev/vdd: 1181.1 GB, 1181116006400 bytes, 2306867200 sectors
 
+# 3. Disk label type 值为dos表示MBR分区, 值为gpt表示GPT分区
+Disk label type: dos
+
