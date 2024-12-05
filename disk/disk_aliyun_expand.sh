628ad1642cacf67785dad8b9e15fc05a78dcee66
zzz <xaozhuge@163.com>
2024-12-05 11:15:13 +0800

    disk_aliyun_expand.sh

--- disk/disk_aliyun_expand.sh
+++ disk/disk_aliyun_expand.sh
@@ -3,3 +3,6 @@ type growpart || sudo yum install -y cloud-utils-growpart
 sudo yum update cloud-utils-growpart
 sudo LC_ALL=en_US.UTF-8 growpart /dev/vdb 1
 
+# 2. 扩容文件系统
+sudo resize2fs /dev/vdb1
+
