1f2b90dec4dfdba8ff6e207e01a5267146dd567d
zzz <xaozhuge@163.com>
2024-12-06 09:27:30 +0800

    disk_aliyun_expand.sh

--- disk/disk_aliyun_expand.sh
+++ disk/disk_aliyun_expand.sh
@@ -1,5 +1,6 @@
 # 1. 扩容分区
 # growpart 是一个用于扩展(增加)分区大小的工具, 用于自动扩展磁盘分区大小
+# 先检查 growpart 命令是否已安装, 如果没有安装, 则自动使用 yum 安装所需的包
 type growpart || sudo yum install -y cloud-utils-growpart
 sudo yum update cloud-utils-growpart
 sudo LC_ALL=en_US.UTF-8 growpart /dev/vdb 1
