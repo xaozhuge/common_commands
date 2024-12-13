2788c4ea86eff8cef283445c3fa234d3e14b6b58
zzz <xaozhuge@163.com>
2024-12-13 09:32:28 +0800

    mysql_table.sh

--- mysql/mysql_table.sh
+++ mysql/mysql_table.sh
@@ -1,7 +1,8 @@
 # 1. 创建数据表
 CREATE TABLE `数据表名称` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `字符串类型` varchar(50) DEFAULT '' COMMENT '字符串备注',
+  `字符串类型` varchar(50) NOT NULL DEFAULT '' COMMENT '字符串备注',
+  `字符串类型` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '字符串支持表情',
   `日期类型` date DEFAULT NULL COMMENT '时间备注',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
