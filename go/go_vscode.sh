2fa629abaaac25c943660196aead898ee4a3dc11
zzz <xaozhuge@163.com>
2024-12-21 22:52:30 +0800

    go_vscode.sh

--- go/go_vscode.sh
+++ go/go_vscode.sh
@@ -2,3 +2,20 @@
 go install github.com/go-delve/delve/cmd/dlv@latest
 dlv version
 
+# 2. 配置 launch.json
+"configurations": [
+    {
+        "name": "Launch Go Program",
+        "type": "go",
+        "request": "launch",
+        "program": "${workspaceFolder}/main.go",
+        "env": {
+            "SERVICE_NAME": "env变量",//例如 oneid
+        },
+        "args": [
+            "main.go的参数"//例如 web
+        ],
+        "cwd": "${workspaceFolder}"
+    }
+]
+
