8df89cbaa00bab037863cb351549bb555ef1d92b
zzz <xaozhuge@163.com>
2024-12-25 09:28:32 +0800

    php_docker_extension.sh

--- php/php_docker_extension.sh
+++ php/php_docker_extension.sh
@@ -11,3 +11,18 @@ install-php-extensions gmp
 # 3.1 将 redis-5.3.7.tgz拷贝到容器内
 docker cp redis-5.3.7.tgz php73_c:/tmp/extensions
 
+# 3.2 命令行执行 installExtensionFromTgz
+installExtensionFromTgz()
+{
+    tgzName=$1
+    result=""
+    extensionName="${tgzName%%-*}" 
+    shift 1
+    result=$@
+    mkdir ${extensionName}
+    tar -xf ${tgzName}.tgz -C ${extensionName} --strip-components=1
+    ( cd ${extensionName} && phpize && ./configure ${result} && make ${MC} && make install )
+
+    docker-php-ext-enable ${extensionName}
+}
+
