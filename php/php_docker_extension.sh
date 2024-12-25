8f00b1fc75720db6d427e881a859d09c9e9ab1c1
zzz <xaozhuge@163.com>
2024-12-25 09:28:01 +0800

    php_docker_extension.sh

--- php/php_docker_extension.sh
+++ php/php_docker_extension.sh
@@ -7,3 +7,7 @@ php -m | grep gmp
 install-php-extensions bcmath
 install-php-extensions gmp
 
+# 3. docker内安装复杂extension, 例如redis
+# 3.1 将 redis-5.3.7.tgz拷贝到容器内
+docker cp redis-5.3.7.tgz php73_c:/tmp/extensions
+
