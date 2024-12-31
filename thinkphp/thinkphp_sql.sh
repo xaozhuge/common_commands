203e25d9c5354eea643af66cbbf1c19fe8833199
zzz <xaozhuge@163.com>
2024-12-31 09:44:56 +0800

    thinkphp_sql.sh

--- thinkphp/thinkphp_sql.sh
+++ thinkphp/thinkphp_sql.sh
@@ -1,2 +1,11 @@
 # 1. 编辑文件 ThinkPHP/Library/Think/Db/Driver.class.php
 
+# 2. 新增方法
+public function logsql($sql, $start_time){
+    $log['sql'] = $sql;
+    $log['sp_time'] = nowms() - $start_time;
+    $log['log_user'] = defined('LOG_USER') ? LOG_USER: '';
+    $log['method'] = INTERFACE_NAME;
+    logs(returnJson($log), 'sql');
+}
+
