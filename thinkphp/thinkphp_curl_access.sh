82b6aaa6d78b1d535c556f73af6c1750363d1667
zzz <xaozhuge@163.com>
2025-01-01 20:08:27 +0800

    thinkphp_curl_access.sh

--- thinkphp/thinkphp_curl_access.sh
+++ thinkphp/thinkphp_curl_access.sh
@@ -1,2 +1,12 @@
 # 1. 打开文件, 包含 curl_exec() 的文件
 
+# 2. 新增方法
+public function logCurlAccess($url, $start_time){
+    $end_time = nowms();
+    $log['url'] = $url;
+    $log['req_time'] = $end_time - $start_time;
+    $log['log_user'] = defined('LOG_USER') ? LOG_USER: '';
+    $log['method'] = INTERFACE_NAME;
+    logs(returnJson($log), 'curl_access');
+}
+
