deb5dd916817d3f5bab9fdd3fa7d8752f791478a
zzz <xaozhuge@163.com>
2025-01-02 09:25:15 +0800

    laravel_sql.sh

--- laravel/laravel_sql.sh
+++ laravel/laravel_sql.sh
@@ -9,3 +9,10 @@
 # 2. vi config/app.php, 新增
 'sql_debug'=>env('SQL_DEBUG', true),
 
+# 3. vi app/Providers/EventServiceProvider.php
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Database\Events\TransactionCommitted;
+use Illuminate\Database\Events\TransactionRolledBack;
+use Illuminate\Database\Events\TransactionBeginning;
+use App\Listeners\QueryExecutedListener;
+
