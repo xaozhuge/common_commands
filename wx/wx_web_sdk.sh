08c29ebfa53590fe9d70bdc21fec8f31538896fe
zzz <xaozhuge@163.com>
2025-01-04 12:29:34 +0800

    wx_web_sdk.sh

--- wx/wx_web_sdk.sh
+++ wx/wx_web_sdk.sh
@@ -1,3 +1,6 @@
 # 1. 微信网页开发(JS-SDK说明文档)
 https://developers.weixin.qq.com/doc/offiaccount/OA_Web_Apps/JS-SDK.html
 
+# 2. 分享给朋友
+https://developers.weixin.qq.com/doc/offiaccount/OA_Web_Apps/JS-SDK.html#111
+
