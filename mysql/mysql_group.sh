bcfc3b354a41bd1a44ce9c5389c7a6860ba8957a
zzz <xaozhuge@163.com>
2025-01-12 19:55:54 +0800

    mysql_group.sh

--- mysql/mysql_group.sh
+++ mysql/mysql_group.sh
@@ -1,6 +1,9 @@
-# 1. group之后 按照排序 取字段
+# 1. group_concat 使用
+GROUP_CONCAT([DISTINCT] 字段 [order by 排序字段 ASC/DESC ] [Separator '分隔符'])
+
+# 2. group之后 按照排序 取字段
 GROUP_CONCAT(id ORDER BY time DESC)
 
-# 2. 取第一个数据
+# 3. 取第一个数据
 substring_index(GROUP_CONCAT(id ORDER BY time DESC), ',' , 1) id
 
