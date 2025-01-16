59cbfb4b1f87436e6d62b4b9f1f2c07c0d96d38c
zzz <xaozhuge@163.com>
2025-01-16 13:01:48 +0800

    php_simulate_curl.php

--- php/php_simulate_curl.php
+++ php/php_simulate_curl.php
@@ -3,6 +3,23 @@
 // 运行命令
 // php ~/ops_code/common_commands/php/php_simulate_curl.php
 
+// 日志配置
+$id="";
+
+// 域名配置
+$map = array(
+    
+);
+
+// DB配置
+$servername="";
+$port=;
+$dbname="";
+$username="";
+$password="";
+$sql="";
+
+
 
 
 ?>
