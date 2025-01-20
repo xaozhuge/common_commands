71b97b899fb4293661df1e39ced88d78f3ed6859
zzz <xaozhuge@163.com>
2025-01-20 09:27:05 +0800

    php_log_l.php

--- php/php_log_l.php
+++ php/php_log_l.php
@@ -7,5 +7,14 @@ $req=<<<EOF
 [2025-01-16 17:26:07] prod.INFO: {"method":"POST","url":"https:\/\/  
 EOF;
 
+$info = explode('prod.INFO: ', $req);
+$post = $info[1];
+$post = json_decode($post, true);
+$url =  $post['url'];
+$param =  $post['param'];
+$header = json_decode($post['req_header'], true);
+$content_type = $header['content-type'][0];
+$authorization = $header['authorization'][0];
+
 
 ?>
