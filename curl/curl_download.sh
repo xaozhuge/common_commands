126fcfe32ec480d2ffd0402e1ca13ad6b0009692
zzz <xaozhuge@163.com>
2025-01-23 09:25:39 +0800

    curl_download.sh

--- curl/curl_download.sh
+++ curl/curl_download.sh
@@ -1,5 +1,8 @@
 # 1. 使用 -O 选项, curl 会下载文件并保留其原始文件名
+# 输出写到该文件中, 保留远程文件的文件名
+# --remote-name
 curl -O http://example.com/file.txt
 
 # 2. 使用 -o 选项, curl 可以将下载的文件保存为指定的文件名
 curl -o myfile.txt http://example.com/file.txt
+
