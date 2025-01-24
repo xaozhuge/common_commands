eac0dccb2c13ddb44921debaed27b41924c69b3f
zzz <xaozhuge@163.com>
2025-01-24 16:10:48 +0800

    nginx_cli.sh

--- nginx/nginx_cli.sh
+++ nginx/nginx_cli.sh
@@ -1,10 +1,12 @@
 # 1. nginx 重新加载配置
 sudo nginx -s reload
+
 # 2. 启动
 /etc/init.d/nginx start
 # 3. 停止运行
 /etc/init.d/nginx stop
 # 4. 重启
 /etc/init.d/nginx restart
-
+# 5. 重新加载配置
+/etc/init.d/nginx reload
 
