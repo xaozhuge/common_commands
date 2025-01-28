044805cb26bacb6d584d25004699e0c7e86eb408
zzz <xaozhuge@163.com>
2025-01-28 09:21:40 +0800

    go_install.sh

--- go/go_install.sh
+++ go/go_install.sh
@@ -5,3 +5,6 @@ https://go.dev/dl/
 # Mac 根据芯片选择 x86-64 还是 ARM64
 # 例如 选择 https://go.dev/dl/go1.22.0.darwin-amd64.tar.gz
 
+# 3. 解压缩并指定解压的目标目录
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.0.darwin-amd64.tar.gz
+
