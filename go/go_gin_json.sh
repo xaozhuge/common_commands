81e7684b8e0d1b2995e58c25af1c521df2914e74
zzz <xaozhuge@163.com>
2025-02-09 18:27:06 +0800

    go_gin_json.sh

--- go/go_gin_json.sh
+++ go/go_gin_json.sh
@@ -33,3 +33,8 @@ type JsonErrStruct struct {
 	Msg  interface{} `json:"msg"`
 }
 
+func ReturnSuccess(c *gin.Context, code int, msg interface{}, data interface{}, count int64){
+	json := &JsonStruct{Code: code, Msg: msg, Data: data, Count: count}
+	c.JSON(200, json)
+}
+
