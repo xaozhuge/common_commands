d47462c749ec63cdfc75afb6a382b0980a3d1949
zzz <xaozhuge@163.com>
2025-03-02 14:26:39 +0800

    go_defer_recover_panic.sh

--- go/go_defer_recover_panic.sh
+++ go/go_defer_recover_panic.sh
@@ -12,4 +12,11 @@ panic("11")
 # 3. recover: 让系统崩溃流程恢复过来, 并且只在 延迟函数defer 中有效
 # 在正常的流程中调用 recover 会返回nil
 # 在有问题时, 可以捕获到panic的输入值，并且恢复正常执行
+defer func(){
+	if err := recover(); err != nil{
+		fmt.Println("捕获异常:", err)
+	}
+}()
+panic("11")
+res: 3 2 1 捕获异常:11
 
