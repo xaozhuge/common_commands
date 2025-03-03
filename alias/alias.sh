2c960aa2fdeed5fe0af44259f5449c97721ead6b
zzz <xaozhuge@163.com>
2025-03-03 21:00:05 +0800

    alias.sh

--- alias/alias.sh
+++ alias/alias.sh
@@ -3,6 +3,7 @@ alias sublime='/usr/bin/open -a /Applications/zzzApp/Sublime*Text.app'
 
 # 2. 定义root密码
 alias root='echo 123456 | sudo -S echo "success"'
+
 # 3. du的权限问题
 alias du1='du -sh * 2>/dev/null | sort -h'
 
