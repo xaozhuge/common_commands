fb1fde78f10c7c60686b99172f5a821ebd9302c4
zzz <xaozhuge@163.com>
2025-03-11 09:29:31 +0800

    kafka.sh

--- kafka/kafka.sh
+++ kafka/kafka.sh
@@ -1,5 +1,7 @@
 # 1. 列出topic, 用于列出 Apache Kafka 集群中所有主题(topics)的命令
 ./kafka-topics.sh --list --bootstrap-server 173.16.0.62:9092
+# ./kafka-topics.sh: 这是 Kafka 提供的一个命令行工具脚本, 用于管理 Kafka 主题(如创建、删除、列出主题等)
+# 通常位于 Kafka 安装目录的 bin 文件夹下
 
 # 2. 创建topic
 ./kafka-topics.sh --bootstrap-server 173.16.0.62:9092 --create --topic example
