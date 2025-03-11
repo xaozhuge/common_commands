d012f5da7063f53f0c160a7c80db17d0cdb50e1c
zzz <xaozhuge@163.com>
2025-03-11 09:25:05 +0800

    kafka.sh

--- kafka/kafka.sh
+++ kafka/kafka.sh
@@ -1,5 +1,6 @@
 # 1. 列出topic
 ./kafka-topics.sh --list --bootstrap-server 173.16.0.62:9092
+
 # 2. 创建topic
 ./kafka-topics.sh --bootstrap-server 173.16.0.62:9092 --create --topic example
 # 3. 从latest位移位置开始消费该主题的所有分区消息，即仅消费正在写入的消息
