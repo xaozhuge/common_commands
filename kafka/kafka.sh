3ba93f649d7581d7853b71765b3a73c892d9761c
zzz <xaozhuge@163.com>
2025-03-17 16:50:13 +0800

    kafka.sh

--- kafka/kafka.sh
+++ kafka/kafka.sh
@@ -1,5 +1,3 @@
-# 2. 创建topic
-./kafka-topics.sh --bootstrap-server 173.16.0.62:9092 --create --topic example
 # 3. 从latest位移位置开始消费该主题的所有分区消息，即仅消费正在写入的消息
 ./kafka-console-consumer.sh --bootstrap-server 173.16.0.62:9092 --topic example
 # 4. 从开始位置消费，并写入文件
