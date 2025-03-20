12531aaf301a4298da19fff6daa149cb3b45f1de
zzz <xaozhuge@163.com>
2025-03-20 10:02:16 +0800

    kafka_ops.sh

--- kafka/kafka_ops.sh
+++ kafka/kafka_ops.sh
@@ -2,7 +2,10 @@
 cd /data/kafka_2.13-3.1.1/
 sh bin/zookeeper-server-start.sh config/zookeeper.properties &
 
-# 2. 启动 kafka
+# 2. 关闭 zookeeper
+sh /data/kafka_2.13-3.1.1/bin/zookeeper-server-stop.sh
+
+# 3. 启动 kafka
 cd /data/kafka_2.13-3.1.1/
 sh bin/kafka-server-start.sh config/server.properties > kafka.log
 
