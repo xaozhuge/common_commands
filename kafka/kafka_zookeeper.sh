64129596a0c885533c7ea690937f7446df1d66bc
zzz <xaozhuge@163.com>
2025-03-20 10:03:54 +0800

    kafka_zookeeper.sh

--- kafka/kafka_zookeeper.sh
+++ kafka/kafka_zookeeper.sh
@@ -2,4 +2,5 @@
 cd /data/kafka_2.13-3.1.1
 sh bin/zookeeper-server-start.sh config/zookeeper.properties &
 # 1. 该命令用于启动 Kafka 的 Zookeeper 服务, 并指定配置文件. 
+# 通过将命令放到后台运行, 可以避免占用当前终端. 启动后, 需要检查日志和端口, 确保 Zookeeper 正常运行
 
