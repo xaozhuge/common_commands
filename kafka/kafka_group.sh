82d00c04df18329bfa9b9923c347756fcc4c8d7c
zzz <xaozhuge@163.com>
2025-03-26 09:32:22 +0800

    kafka_group.sh

--- kafka/kafka_group.sh
+++ kafka/kafka_group.sh
@@ -5,3 +5,7 @@ bin/kafka-consumer-groups.sh --bootstrap-server 192.168.0.31:19092 --list
 # 1.1. 这是Kafka自带的用于管理消费者组的脚本工具
 bin/kafka-consumer-groups.sh
 
+# 1.2 指定要连接的 Kafka 集群地址
+# 这里连接的IP是 192.168.0.31, 端口为 19092 的Kafka服务
+--bootstrap-server 192.168.0.31:19092
+
