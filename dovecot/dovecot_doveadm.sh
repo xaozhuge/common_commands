9218e6c861ecbda4809332c157f88ce9cad87df4
zzz <xaozhuge@163.com>
2025-03-31 09:29:17 +0800

    dovecot_doveadm.sh

--- dovecot/dovecot_doveadm.sh
+++ dovecot/dovecot_doveadm.sh
@@ -5,3 +5,6 @@ doveadm fetch -u 用户@域名 "uid" mailbox INBOX all
 # 2. 获取 指定(用户)邮箱的 所有邮件键(uid)、邮件名前半部分
 doveadm fetch -u 用户@域名 "uid guid" mailbox INBOX all
 
+# 3. 获取 指定(用户)邮箱 并且 指定邮件uid 的 body(正文)
+doveadm fetch -u 用户@域名 "body" mailbox INBOX uid
+
