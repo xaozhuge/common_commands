d1c8e94fe84646341fffb5876ee014b771d8a37d
zzz <xaozhuge@163.com>
2025-03-31 09:29:31 +0800

    dovecot_doveadm.sh

--- dovecot/dovecot_doveadm.sh
+++ dovecot/dovecot_doveadm.sh
@@ -7,4 +7,7 @@ doveadm fetch -u 用户@域名 "uid guid" mailbox INBOX all
 
 # 3. 获取 指定(用户)邮箱 并且 指定邮件uid 的 body(正文)
 doveadm fetch -u 用户@域名 "body" mailbox INBOX uid
+# 正文的内容做了base64处理, 并且字符集是gb18030, 需要做特殊处理, 例如python处理
+import base64
+print(base64.b64decode("xOO6wy3V/c7E").decode('gb18030'))
 
