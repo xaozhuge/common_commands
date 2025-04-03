9c468bf845914fb6ad3bbf5cc2cfe60e3f6e6906
zzz <xaozhuge@163.com>
2025-04-03 19:06:51 +0800

    kafka_group.sh

--- kafka/kafka_group.sh
+++ kafka/kafka_group.sh
@@ -1,7 +1,6 @@
 # 1. 列出Kafka中所有的消费者组(consumer groups)
 cd /data/kafka_2.13-3.1.1/
 bin/kafka-consumer-groups.sh --bootstrap-server 192.168.0.31:19092 --list
-
 # 1.1. 这是Kafka自带的用于管理消费者组的脚本工具
 bin/kafka-consumer-groups.sh
 
