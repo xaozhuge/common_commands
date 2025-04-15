35e14daeaf54ec4de6b7df70efe37d97b062f406
zzz <xaozhuge@163.com>
2025-04-15 17:31:17 +0800

    jenkins_laravel.sh

--- jenkins/jenkins_laravel.sh
+++ jenkins/jenkins_laravel.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
-
 echo '开始更新代码' 
 ssh -tt 用户名@IP "pwd;\
 cd /data/www/api_code;\
 sudo /bin/git pull origin master;\
 sudo /bin/docker exec php73_c php /www/api_code/artisan route:clear;\
 sudo /bin/docker exec php73_c php /www/api_code/artisan config:clear;\
+ls -al --full-time|grep git|grep -v .gitignore;"
+echo '结束更新代码' 
+echo ''
+
+#!/bin/sh
+echo '开始更新代码' 
+ssh -tt 用户名@IP "pwd;\
+cd /data/www/api_code;\
+sudo /bin/git pull origin master;\
+sudo /alidata/server/php73/bin/php artisan config:clear;\
+sudo /alidata/server/php73/bin/php artisan route:clear;\
 ls -al --full-time|grep git|grep -v .gitignore;"
 echo '结束更新代码' 
 echo ''
