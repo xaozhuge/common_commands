09923a11d9c100f527cc61590006308cf8512883
zzz <xaozhuge@163.com>
2025-04-17 13:32:31 +0800

    kafka_group.sh

--- kafka/kafka_group.sh
+++ kafka/kafka_group.sh
@@ -2,12 +2,13 @@
 # 1. 这是Kafka自带的用于管理消费者组的脚本工具
 bin/kafka-consumer-groups.sh
 
+# 2. 指定要连接的 Kafka 集群地址
+# 这里连接的IP是 192.168.0.31, 端口为 19092 的Kafka服务
+--bootstrap-server 192.168.0.31:19092
+
 # 1. 列出Kafka中所有的消费者组(consumer groups)
 cd /data/kafka_2.13-3.1.1/
 bin/kafka-consumer-groups.sh --bootstrap-server 192.168.0.31:19092 --list
-# 1.2 指定要连接的 Kafka 集群地址
-# 这里连接的IP是 192.168.0.31, 端口为 19092 的Kafka服务
---bootstrap-server 192.168.0.31:19092
 # 1.3 表示要执行的操作是列出所有消费者组
 --list
 # 1.4 执行此命令后, 会返回该 Kafka 集群中所有的消费者组名称列表
