7e573a59cc737b12914ea1c191e7b44c92916245
zzz <xaozhuge@163.com>
2025-04-26 22:27:29 +0800

    kafka_topic.sh

--- kafka/kafka_topic.sh
+++ kafka/kafka_topic.sh
@@ -1,10 +1,10 @@
 # 一、参数说明
 # 1. kafka-topics.sh 这是 Kafka 提供的一个命令行工具脚本, 用于管理 Kafka 主题(如创建、删除、列出主题等)
+# Create, delete, describe, or change a topic
 bin/kafka-topics.sh
 
 
 
-# Create, delete, describe, or change a topic
 # 通常位于 Kafka 安装目录的 bin 文件夹下
 cd /data/kafka_2.13-3.1.1/bin/
 ./kafka-topics.sh
