9405bfcf9d8d261b0a8212289e2593e35481e5e3
zzz <xaozhuge@163.com>
2025-05-06 10:31:08 +0800

    curl_ms.sh

--- curl/curl_ms.sh
+++ curl/curl_ms.sh
@@ -4,3 +4,7 @@ JSON='{"page": 1, "num": 20}';
 HEADER1="Content-Type: application/json";
 curl -X POST "$URL" -H "$HEADER1" -d "$JSON";
 
+# 2. 多header时
+HEADER1="Content-Type: application/x-www-form-urlencoded";
+HEADER2="Authorization: token";
+
