d4b2949f26f62ca445d98efce92449f4a6d790c9
zzz <xaozhuge@163.com>
2025-05-10 22:58:20 +0800

    laravel_serviceprovider.sh

--- laravel/laravel_serviceprovider.sh
+++ laravel/laravel_serviceprovider.sh
@@ -1,3 +1,6 @@
 # 1. 在Laravel中, ServiceProvider是框架的核心组件之一, 用于服务注册和启动逻辑的引导
 # 几乎所有Laravel的核心服务(如数据库、队列、缓存、认证等)都是通过 ServiceProvider 注册进来的
 
+# 2. 什么是 ServiceProvider?
+# ServiceProvider 是 Laravel 用来将服务(类、功能模块)注册到服务容器(IoC Container)中的机制
+
