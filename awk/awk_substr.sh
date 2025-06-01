625d5bbb9775a310a9bb0d0ed5ae15cd6b25a326
zzz <xaozhuge@163.com>
2025-06-01 22:38:38 +0800

    awk_substr.sh

--- awk/awk_substr.sh
+++ awk/awk_substr.sh
@@ -13,3 +13,7 @@ tail -n 10 2025-05-30.log | awk '{ print $5 }'
 # 对输出的结果进行排序, 对相邻的数据进行去重统计，按照统计数目由大到小排序
 cat 2025-05-30.log|awk '{print $5}'|awk '{print substr($1,2,18)}' |sort |uniq -c|sort -nr
 
+# 每一行都执行打印第5列, 每一行都对第一列执行从第2位开始,截取18位,并赋值给变量key
+# 每一列都执行以key作为键值的计数, 打印key的数目和键值, 按照统计数目由大到小排序
+cat 2025-05-30.log|awk '{print $5}'|awk '{key=substr($1,2,18);count[key]++;print count[key]"  "key}' |sort -nr
+
