82d7711b36eb283e0d16fa729daf45b0c5657a00
zzz <xaozhuge@163.com>
2025-06-05 09:32:54 +0800

    script_check.sh

---
+++ script/script_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source /etc/profile
+
+# 定义函数
+notice() {
+	local key=$1
+	local content=$2
+	remind=
+	URL=https://域名.com/im/robot/send?key=$key
+	JSON="{\"content\": \"$content\", \"remind\": \"$remind\"}"
+	HEADER1="Content-Type: application/json";
+	curl -s -S -X POST "$URL" -H "$HEADER1" -d "$JSON";
+}
+
