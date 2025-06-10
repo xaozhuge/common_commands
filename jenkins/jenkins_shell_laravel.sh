a33985e865f92110420d47112b568d6401370448
zzz <xaozhuge@163.com>
2025-06-10 16:11:32 +0800

    jenkins_shell_laravel.sh

--- jenkins/jenkins_shell_laravel.sh
+++ jenkins/jenkins_shell_laravel.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 echo '开始更新' 
 ssh -tt 用户名@IP "pwd;\
 cd 项目目录;\
@@ -9,4 +8,16 @@ sudo /bin/docker exec php73_c php /www/项目目录/artisan config:clear;\
 ls -al --full-time|grep git|grep -v .gitignore;"
 echo '结束更新' 
 echo ''
+
+
+
+#!/bin/sh
+echo '开始更新' 
+ssh -tt 用户名@IP "pwd;\
+cd 项目目录;\
+sudo /bin/git pull origin master --quiet;\
+sudo /alidata/server/php73/bin/php artisan config:clear;\
+sudo /alidata/server/php73/bin/php artisan route:clear;\
+ls -al --full-time|grep git|grep -v .gitignore;"
+echo '结束更新' 
 
