616af31533a38fff15fb6dbfbea8ac590a6ce755
zzz <xaozhuge@163.com>
2025-06-11 15:11:09 +0800

    script_curl.sh

--- script/script_curl.sh
+++ script/script_curl.sh
@@ -11,3 +11,13 @@ curl1() {
 # 调用函数
 curl1 "URL" "TOKEN" "param1=1&param2=2"
 
+# 定义函数
+curl2() {
+  local URL=$1
+  local TOKEN=$2
+  local DATA=$3
+  HEADER1="Content-Type: application/json";
+  HEADER2="authorization: $TOKEN";
+  curl -s -S -X POST "$URL" -H "$HEADER1" -H "$HEADER2" -d "$DATA";
+}
+
