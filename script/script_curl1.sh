7a7a5d7ca13d44a1b0fa3c401b584cf445b3f60f
zzz <xaozhuge@163.com>
2025-06-12 09:51:06 +0800

    script_curl1.sh

---
+++ script/script_curl1.sh
@@ -0,0 +1,15 @@
+# 定义函数
+curl1() {
+  local URL=$1
+  local TOKEN=$2
+  local DATA=$3
+  HEADER1="Content-Type: application/x-www-form-urlencoded";
+  HEADER2="authorization: $TOKEN";
+  # curl -s -S -X POST "$URL" -H "$HEADER1" -H "$HEADER2" -d "$DATA";
+cmd=$(cat <<EOF
+curl -s -S -X POST "$URL" -H "$HEADER1" -H "$HEADER2" -d '$DATA';
+EOF
+)
+  echo "$cmd"
+}
+
