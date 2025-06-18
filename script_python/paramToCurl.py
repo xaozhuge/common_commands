fd3a2512a6c0fea28b2c3ed1138a50364f91da62
zzz <xaozhuge@163.com>
2025-06-18 09:34:02 +0800

    paramToCurl.py

---
+++ script_python/paramToCurl.py
@@ -0,0 +1,13 @@
+import time
+import json
+import pymysql
+from pymysql.cursors import DictCursor
+from urllib.parse import urlencode
+
+id=
+
+# 输出后退出
+def p(content):
+    print(content)
+    exit()
+
