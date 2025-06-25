72db9a658ccac6adb87fbea1a8b84088030f2352
zzz <xaozhuge@163.com>
2025-06-25 13:27:08 +0800

    calbysecond.py

--- script_python/calbysecond.py
+++ script_python/calbysecond.py
@@ -16,3 +16,9 @@ def toTimestamp(time_str):
     ts = datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S")
     return int(ts.timestamp())
 
+# 当天的某个时间转化为时间戳
+def currentDayToTimestamp(fixed_time_str):
+    today_str = datetime.now().strftime("%Y-%m-%d")
+    full_datetime_str = f"{today_str} {fixed_time_str}"
+    return toTimestamp(full_datetime_str)
+
