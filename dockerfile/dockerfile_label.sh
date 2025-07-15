a21112b71544dd89512ff1188041505ed559ed2c
zzz <xaozhuge@163.com>
2025-07-15 10:15:06 +0800

    dockerfile_label.sh

--- dockerfile/dockerfile_label.sh
+++ dockerfile/dockerfile_label.sh
@@ -4,3 +4,9 @@ LABEL stage=builder_${APP_NAME}
 # 作用: 给镜像添加可查询的元信息(给镜像打标签以便识别或分层处理)
 # 个人理解: 为生成的 Docker 镜像添加自定义的键值对标签
 
+
+LABEL stage=builder_${APP_NAME}
+# 2. `LABEL` 是 Dockerfile 中的指令, 用来向镜像添加键值对元数据
+# `stage` 是标签名(key)
+# `builder_${APP_NAME}` 是标签值
+
