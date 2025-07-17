cd0a4f5b0215bc238d696fc2069c16ffc7db761b
zzz <xaozhuge@163.com>
2025-07-17 13:53:31 +0800

    dockerfile_run.sh

--- dockerfile/dockerfile_run.sh
+++ dockerfile/dockerfile_run.sh
@@ -5,3 +5,6 @@ RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositorie
 	# 提高构建或更新 Alpine 包的速度
 	# 避免访问国外源时超时或失败的问题
 
+RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
+# 2.1 RUN: Dockerfile 中用于执行一条命令. 表示这是在构建镜像过程中运行的操作
+
