635e87e6de12d63ac3a0ed7b0afb24b4af6880eb
zzz <xaozhuge@163.com>
2025-07-18 09:22:45 +0800

    dockerfile_run.sh

--- dockerfile/dockerfile_run.sh
+++ dockerfile/dockerfile_run.sh
@@ -9,4 +9,7 @@ RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositorie
 # 2.1 RUN: Dockerfile 中用于执行一条命令. 表示这是在构建镜像过程中运行的操作
 # 2.2 sed: 是一个流编辑器, 用于查找和替换文本
 # 2.3 -i: 表示直接修改文件, 而不是将结果输出到标准输出
+# 2.4 s/.../.../g: 是 sed 的替换命令, 格式为：`s/原字符串/新字符串/g`
+	# `g` 表示全局替换
+	# 替换后, `apk add`、`apk update` 等命令在下载软件时会走国内源, 速度更快, 更稳定
 
