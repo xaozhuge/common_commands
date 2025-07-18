45dd358a0a2ed76f4faf728248f5c3fd7d8a5eea
zzz <xaozhuge@163.com>
2025-07-18 09:22:13 +0800

    dockerfile_run.sh

--- dockerfile/dockerfile_run.sh
+++ dockerfile/dockerfile_run.sh
@@ -8,4 +8,5 @@ RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositorie
 RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
 # 2.1 RUN: Dockerfile 中用于执行一条命令. 表示这是在构建镜像过程中运行的操作
 # 2.2 sed: 是一个流编辑器, 用于查找和替换文本
+# 2.3 -i: 表示直接修改文件, 而不是将结果输出到标准输出
 
