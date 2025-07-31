b25c1c518ca9eb385fc984750fa325ab6babba6a
zzz <xaozhuge@163.com>
2025-07-31 16:27:20 +0800

    redis.sh

--- redis/redis.sh
+++ redis/redis.sh
@@ -11,5 +11,6 @@ redis-cli -h 127.0.0.1 -p 6379
 /alidata/server/redis-3.0.2/src/redis-cli shutdown
 
 # 5. MEMORY USAGE 是 Redis 提供的用于查看某个键(key)在 Redis 中占用的内存大小(以字节为单位)
+# redis_key_name 支持是 list 的key值
 MEMORY USAGE redis_key_name
 
