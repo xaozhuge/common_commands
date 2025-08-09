5a57d1297557c7f27ea3cdcc0c6b3a137ab7067b
zzz <xaozhuge@163.com>
2025-08-09 23:36:50 +0800

    kafka_understand.sh

--- kafka/kafka_understand.sh
+++ kafka/kafka_understand.sh
@@ -2,4 +2,6 @@
 Kafka 采用 ​消费者主动拉取(Pull)而非服务端推送(Push), 但通过优化实现了接近实时通知的效果
 
 # 2. kafka 拉取数据-消费者向 Broker 发送 ​FetchRequest​ 请求数据
+# 2.1 无数据时的行为​
+# Broker 不会立即返回空响应, 而是 ​阻塞等待​ (默认最多 max.poll.interval.ms, 如 5 秒), 直到新数据到达或超时
 
