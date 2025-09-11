b5766ae180426776eeae31c01440ba8b05168a5d
zzz <xaozhuge@163.com>
2025-09-11 09:10:15 +0800

    lua_log.sh

--- lua/lua_log.sh
+++ lua/lua_log.sh
@@ -2,3 +2,6 @@
 # 这行代码的作用是 当 Lua 脚本操作 MySQL 出错时, 把错误信息以 error级别 写进 Nginx 的 error.log 文件中
 ngx.log(ngx.ERR, "new mysql error : " .. err)
 
+# 2. ngx.log(level, ...): 这是 OpenResty 提供的 API, 用来往 Nginx 的 error.log 里写日志
+ngx.log(ngx.ERR, "new mysql error : " .. err)
+
