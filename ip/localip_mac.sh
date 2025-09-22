7532fe8cd428ab6443b10fa7f61526ae638b085e
zzz <xaozhuge@163.com>
2025-09-22 10:47:57 +0800

    localip_mac.sh

--- ip/localip_mac.sh
+++ ip/localip_mac.sh
@@ -6,9 +6,11 @@ timeout=1  # 超时时间(秒)
 echo "开始扫描服务器端口..."
 echo "========================="
 
+# 循环1-254
 for i in {1..254}; do
 	server="172.16.1.${i}"
     
+    # 利用 curl 判断端口是否开启
     res=`curl -sS --connect-timeout $timeout $server:$port 2>&1`
 
 	if echo "$res" | grep -q "Timeout was reached"; then
@@ -16,6 +18,7 @@ for i in {1..254}; do
 	else
 		res=`nc -vz $server $port 2>&1`
 
+		# 使用 nc 判断端口是否有服务运行
 		if echo "$res" | grep -q "succeeded"; then
 			echo "服务器IP: $server succeeded"
 		else
