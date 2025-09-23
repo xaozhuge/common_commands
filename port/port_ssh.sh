bcefbd88abe2cb15c72187d84f0f55963b348a59
zzz <xaozhuge@163.com>
2025-09-23 10:27:10 +0800

    port_ssh.sh

--- port/port_ssh.sh
+++ port/port_ssh.sh
@@ -4,3 +4,6 @@ ssh -p 端口 -o ConnectTimeout=1 root@IP
 # 2. 如果端口未开放
 ssh: connect to host 172.16.1.69 port 21: Operation timed out
 
+# 3. 如果端口开放, 但是无服务
+ssh: connect to host 172.16.1.169 port 80: Connection refused
+
