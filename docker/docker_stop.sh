4be7d982a17d5fb1a7350e80cf824942f03aac88
zzz <xaozhuge@163.com>
2025-09-26 13:47:55 +0800

    docker_stop.sh

--- docker/docker_stop.sh
+++ docker/docker_stop.sh
@@ -8,3 +8,6 @@ sudo systemctl stop docker
 # 是一个 Systemd 套接字单元, 监听 Docker 的 Unix 套接字 (/var/run/docker.sock). 
 # 当客户端(如 docker 命令)尝试连接时, 会自动启动 docker.service
 ​
+# 4. 现象分析​
+# 即使你手动停止 docker.service, 只要 docker.socket 仍在运行, 后续任何调用 docker 命令的操作都会重新激活 Docker 服务
+
