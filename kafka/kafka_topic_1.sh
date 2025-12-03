bfcc79b5f36de6095db4fa51a6482f04ca8042f0
zzz <xaozhuge@163.com>
2025-12-03 22:58:07 +0800

    kafka_topic_1.sh

--- kafka/kafka_topic_1.sh
+++ kafka/kafka_topic_1.sh
@@ -1,2 +1,7 @@
 # 创建一个名为 ‘test-topic’ 的主题, 1个分区, 1个副本
+$KAFKA_HOME/bin/kafka-topics.sh --create \
+  --bootstrap-server localhost:9092 \ # Kafka 服务地址
+  --replication-factor 1 \
+  --partitions 1 \
+  --topic test-topic
 
