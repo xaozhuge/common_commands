860f24c95fea33209cb6f306ef4971e4531fb1f5
zzz <xaozhuge@163.com>
2025-12-11 16:54:08 +0800

    mac.sh

--- mac/mac.sh
+++ mac/mac.sh
@@ -4,3 +4,6 @@ launchctl load ~/Library/LaunchAgents/homebrew.mxcl.php@5.6.plist
 launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.php@5.6.plist
 # 3. mac的环境变量加载顺序
 /etc/profile /etc/paths ~/.bash_profile
+
+# 4. 在Mac的Finder中显示隐藏文件的快捷命令
+
