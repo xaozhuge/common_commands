f45f2fbf580d693927cfed7ce8161990b89e5d4c
zzz <xaozhuge@163.com>
2026-04-09 12:42:42 +0800

    sentry_clean.sh

--- sentry/sentry_clean.sh
+++ sentry/sentry_clean.sh
@@ -13,3 +13,5 @@ psql
 SELECT table_schema || '.' || table_name AS table_full_name, pg_size_pretty(pg_total_relation_size('"' || table_schema || '"."' || table_name || '"')) AS size FROM information_schema.tables ORDER BY
 pg_total_relation_size('"' || table_schema || '"."' || table_name || '"') DESC limit 20;
 
+# 5. 查看表结构
+
