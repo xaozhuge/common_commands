242cbf888a0ab6da6e2b3f6c64c70b77c5672fb4
zzz <xaozhuge@163.com>
2026-04-09 12:42:25 +0800

    sentry_clean.sh

--- sentry/sentry_clean.sh
+++ sentry/sentry_clean.sh
@@ -10,4 +10,6 @@ psql
 
 # 4. 查看表数据
 # 在清理前后可以查看下这个表占用的空间
+SELECT table_schema || '.' || table_name AS table_full_name, pg_size_pretty(pg_total_relation_size('"' || table_schema || '"."' || table_name || '"')) AS size FROM information_schema.tables ORDER BY
+pg_total_relation_size('"' || table_schema || '"."' || table_name || '"') DESC limit 20;
 
