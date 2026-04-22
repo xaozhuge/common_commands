ce5571797c72d4ec344fc8e598f8cdedcaa86794
zzz <xaozhuge@163.com>
2026-04-22 12:53:47 +0800

    openclaw_agent.sh

--- openclaw/openclaw_agent.sh
+++ openclaw/openclaw_agent.sh
@@ -3,4 +3,9 @@ openclaw agents add
 
 # 2. 访问指定agent的数据
 # 使用 HTTP Header, X-OpenClaw-Agent-ID: agent名称
+curl -N http://127.0.0.1:18789/v1/chat/completions \
+ -H 'Content-Type: application/json' \
+ -H 'Authorization: Bearer <变量token>' \
+ -H 'x-openclaw-agent-id: <变量agent>' \
+ -d '{"model": "openclaw", "stream": true, "messages": [{"role":"user","content":"你的worskspace路径是什么"} ]}'
 
