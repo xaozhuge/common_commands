f7a3c738fed3a4138d338c7f6675607847b8aa5e
zzz <xaozhuge@163.com>
2026-05-12 14:18:52 +0800

    hw_obs.sh

--- obs_oss/hw_obs.sh
+++ obs_oss/hw_obs.sh
@@ -8,4 +8,5 @@ uname -m
 # 2. Linux AMD 64位
 wget https://obs-community.obs.cn-north-1.myhuaweicloud.com/obsutil/current/obsutil_linux_amd64.tar.gz
 # 在软件包所在目录, 执行以下解压命令
+tar -xzvf obsutil_linux_amd64.tar.gz
 
