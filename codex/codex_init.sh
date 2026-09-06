ed7907570ccc446698a8617f1ca43398550604f8
zzz <xaozhuge@163.com>
2026-09-06 18:51:39 +0800

    codex_init.sh

--- codex/codex_init.sh
+++ codex/codex_init.sh
@@ -3,4 +3,5 @@ https://openai.com/zh-Hans-CN/codex/
 # 直接在 https://chatgpt.com/projects 里 点击下载也可以
 
 # 2. codex 支持使用自定义的 大模型, 我们可以切换为使用国内的大模型
+# 使用 豆包工作 或者 workbuddy 帮忙将 codex 的模型 配置为 国内的 模型, 例如 deepseek
 
